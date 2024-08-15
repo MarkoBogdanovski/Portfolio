228517bac944d7e7d56f51e70b696c180a8dc7ff
Marko Bogdanovski <markob@sendelemental.com>
2024-08-15 17:25:24 +0200

    Blog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -68,7 +67,6 @@ export default function RootLayout({
             <Navbar />
           </TooltipProvider>
         </ThemeProvider>
-        <SpeedInsights />
       </body>
     </html>
   );
