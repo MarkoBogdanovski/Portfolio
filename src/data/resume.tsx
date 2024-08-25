b221b35990d7e653906ee7986a7cd32d68dad593
Marko Bogdanovski <markob@sendelemental.com>
2024-08-25 21:36:41 +0200

    Add projects

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -51,13 +51,6 @@ export const DATA = {
 
         navbar: true,
       },
-      Behance: {
-        name: "Behance",
-        url: "https://www.behance.net/MarkoBogdanovski",
-        icon: Icons.github,
-
-        navbar: true,
-      },
       email: {
         name: "Send Email",
         url: "#",
@@ -118,126 +111,58 @@ export const DATA = {
   ],
   projects: [
     {
-      title: "Chat Collect",
-      href: "https://chatcollect.com",
-      dates: "Jan 2024 - Feb 2024",
-      active: true,
+      title: "Obba",
+      href: "https://obba.online/",
+      dates: "Sep 2019 - Aug 2020",
+      active: false,
       description:
-        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
+        "Developed multi vendor e-commerce ",
       technologies: [
-        "Next.js",
-        "Typescript",
+        "Laravel",
+        "NuxtJS",
+        "Vuex",
+        "Cypress",
+        "Gitlab CI/CD",
+        "Docker",
         "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Stripe",
-        "Shadcn UI",
-        "Magic UI",
+        "AWS",
+        "Tailwind",
+        "Socket.io"
       ],
       links: [
         {
           type: "Website",
-          href: "https://chatcollect.com",
+          href: "https://www.producthunt.com/new/products/obba-store",
           icon: <Icons.globe className="size-3" />,
         },
       ],
       image: "",
       video:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
+        "https://res.cloudinary.com/dwl9ar0hz/video/upload/f_auto:video,q_auto/w45jzs4k2fvj0pvsesqm",
     },
     {
-      title: "Magic UI",
-      href: "https://magicui.design",
-      dates: "June 2023 - Present",
+      title: "Glorious CMS",
+      href: "https://glorious.free.nf",
+      dates: "February 2016",
       active: true,
       description:
-        "Designed, developed and sold animated UI components for developers.",
+        "Developed back in 2016 for a hackaton for the university scholarship.",
       technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Stripe",
-        "Shadcn UI",
-        "Magic UI",
+        "PHP",
+        "jQuery",
+        "Bootstrap",
+        "MySQL",
       ],
       links: [
         {
           type: "Website",
-          href: "https://magicui.design",
-          icon: <Icons.globe className="size-3" />,
-        },
-        {
-          type: "Source",
-          href: "https://github.com/magicuidesign/magicui",
-          icon: <Icons.github className="size-3" />,
-        },
-      ],
-      image: "",
-      video: "https://cdn.magicui.design/bento-grid.mp4",
-    },
-    {
-      title: "llm.report",
-      href: "https://llm.report",
-      dates: "April 2023 - September 2023",
-      active: true,
-      description:
-        "Developed an open-source logging and analytics platform for OpenAI: Log your ChatGPT API requests, analyze costs, and improve your prompts.",
-      technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Shadcn UI",
-        "Magic UI",
-        "Stripe",
-        "Cloudflare Workers",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://llm.report",
-          icon: <Icons.globe className="size-3" />,
-        },
-        {
-          type: "Source",
-          href: "https://github.com/dillionverma/llm.report",
-          icon: <Icons.github className="size-3" />,
-        },
-      ],
-      image: "",
-      video: "https://cdn.llm.report/openai-demo.mp4",
-    },
-    {
-      title: "Automatic Chat",
-      href: "https://automatic.chat",
-      dates: "April 2023 - March 2024",
-      active: true,
-      description:
-        "Developed an AI Customer Support Chatbot which automatically responds to customer support tickets using the latest GPT models.",
-      technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Shadcn UI",
-        "Magic UI",
-        "Stripe",
-        "Cloudflare Workers",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://automatic.chat",
+          href: "https://glorious.free.nf",
           icon: <Icons.globe className="size-3" />,
         },
       ],
       image: "",
       video:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/automatic-chat.mp4",
+        "https://res.cloudinary.com/dwl9ar0hz/video/upload/f_auto:video,q_auto/rueqfc4o5jssbmfctcf4",
     },
-  ],
+  ]
 } as const;
